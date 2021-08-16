61876c92ad93f507146e9be869e305912911e873
MPB <mp4learning@gmail.com>
2021-08-16 21:20:22 +0530

    Databinding support

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -3,23 +3,28 @@ package com.example.kotlinapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.kotlinapp.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        mBinding =  DataBindingUtil.setContentView(this, R.layout.activity_main)
         add(5, 10)
         addition(12,7)
     }
 
-    fun add(a : Int , b: Int ) {
+    private fun add(a : Int, b: Int ) {
         val sum  = a + b;
         Log.v("sum:", sum.toString())
     }
 
     //function with default argument
-    fun addition(x: Int = 100, y:Int = 20) {
+    private fun addition(x: Int = 100, y:Int = 20) {
         val sum =  x + y;
         Log.v("Sum:", sum.toString())
     }
