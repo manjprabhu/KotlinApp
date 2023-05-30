9bd65ec09ae0bda38aefe33421768e2eeddb9785
MPB <mp4learning@gmail.com>
2023-05-30 11:58:42 +0530

    Scoper function demo

--- app/src/main/java/com/example/kotlinapp/ScopeFunctionDemo.kt
+++ app/src/main/java/com/example/kotlinapp/ScopeFunctionDemo.kt
@@ -8,8 +8,8 @@ class ScopeFunctionDemo : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        performLetOperation()
-        performLetOperationTwo()
+        performApplyOperationWithNull()
+        performRunOperationWithNull()
 
     }
 
@@ -25,10 +25,60 @@ class ScopeFunctionDemo : Activity() {
 
     private fun performLetOperationTwo() {
         val p = Person()
-        val x  = Person().let {
+        val x = Person().let {
             return@let "${it.age}"
         }
+        Log.d("TAG", "==>> Display: $x")
+    }
 
-        Log.d("TAG","==>> Display: $x")
+    private fun performRunOperation() {
+        val p = Person()
+        val x = p.run {
+            name = "QWERT"
+            age = 56
+            contactNumber = 8832
+            return@run "${display()}"
+        }
+        Log.d("TAG", "RUN ==>> $x  ${p.name} ${p.age}  ${p.contactNumber}")
+    }
+
+    private fun performApplyOperation() {
+        val p = Person()
+        val x = p.apply {
+            age = 45
+            name = "APPLY NAME"
+            contactNumber = 4388
+        }
+        Log.d("TAG", "APPLY ==>> ${x.name}")
+    }
+
+    private fun performApplyOperationWithNull() {
+        val p: Person? = null
+        p?.apply {
+            this.name = "NULL NAME"
+        }
+        Log.d("TAG", "APPLY NUll ==>> ${p?.name}")
+    }
+
+    private fun performWithOperationWithNull() {
+        val p: Person? = null
+        with(p) {
+            this?.name = "NULL NAME"
+            this?.age = 10
+            this?.contactNumber = 3288932
+            this?.displayInfo()
+        }
+    }
+
+    private fun performRunOperationWithNull() {
+        val p: Person? = null
+        val x = p?.run {
+            name = "RUN NULL NAME"
+            age = 10
+            contactNumber = 3288932
+            displayInfo()
+        }
+
+        println("==>> $x")
     }
 }
