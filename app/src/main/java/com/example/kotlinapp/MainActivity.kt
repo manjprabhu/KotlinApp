526c6ac83ac04f971b8fe60897bad163e1129e5c
MPB <mp4learning@gmail.com>
2023-06-10 12:23:16 +0530

    Exception handling in Coroutine

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import com.example.kotlinapp.coroutineexceptionhandling.CoroutineexceptionhandlerDemo
 import com.example.kotlinapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val intent = Intent(this, StateFlowDemo::class.java)
+        val intent = Intent(this, CoroutineexceptionhandlerDemo::class.java)
         startActivity(intent)
 //        add(5, 10)
 //        addition(12, 7)
---
+++ app/src/main/java/com/example/kotlinapp/coroutineexceptionhandling/CoroutineexceptionhandlerDemo.kt
@@ -0,0 +1,104 @@
+package com.example.kotlinapp.coroutineexceptionhandling
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+
+class CoroutineexceptionhandlerDemo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        multipleCoroutineOperationTwo()
+    }
+
+    fun performExceptionHandling() {
+
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            println("==>> Caught $throwable by exception handler")
+        }
+
+        val scope = CoroutineScope(Job() + exceptionHandler)
+        scope.launch {
+            throw RuntimeException()
+        }
+    }
+
+    private fun multipleCoroutineOperation() {
+
+        lifecycleScope.launch {
+
+            supervisorScope {
+                val job1 = async { functionOne() }
+
+                val job2 = async { functionTwo() }
+
+                val job3 = async { functionThree() }
+
+                try {
+                    println("==>> Job1 : ${job1.await()}")
+                } catch (e: Exception) {
+                    println("==>> Caught exception in $e")
+                }
+
+                try {
+                    println("==>> Job2 : ${job3.await()}")
+                } catch (e: Exception) {
+                    println("==>> Job2 Caught exception in $e")
+                }
+
+                try {
+                    println("==>> Job3 : ${job2.await()}")
+                } catch (e: Exception) {
+                    println("==>>Job3  Caught exception in $e")
+                }
+            }
+        }
+    }
+
+
+    private fun multipleCoroutineOperationTwo() {
+
+        lifecycleScope.launch {
+
+            supervisorScope {
+                val job1 = async { functionOne() }
+
+                val job2 = async { functionTwo() }
+
+                val job3 = async { functionThree() }
+
+                val result= listOf(job1, job2, job3).mapNotNull {
+                    try {
+                        it.await()
+                    } catch (e: Exception) {
+                        println("==>> Caught exception in $e")
+                        null
+                    }
+                }
+
+                println("==>> Result of computation: $result")
+
+            }
+        }
+    }
+
+    private suspend fun functionOne() {
+        delay(500)
+        throw RuntimeException("functionOne exception")
+    }
+
+    private suspend fun functionTwo(): String {
+        delay(400)
+        return "functionTwo"
+    }
+
+    private suspend fun functionThree(): String {
+        delay(400)
+        return "functionThree"
+    }
+}
