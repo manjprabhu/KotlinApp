a107c3bd7e703a37fc9b685079a7a80f19fb25e0
MPB <mp4learning@gmail.com>
2023-10-30 20:10:48 +0530

    Coroutine context

--- app/src/main/java/com/example/kotlinapp/CoroutineBasics.kt
+++ app/src/main/java/com/example/kotlinapp/CoroutineBasics.kt
@@ -3,10 +3,7 @@ package com.example.kotlinapp
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class CoroutineBasics : AppCompatActivity() {
 
@@ -57,6 +54,7 @@ class CoroutineBasics : AppCompatActivity() {
         }
     }
 
+    //Running 1000 coroutine
     fun demoThree() {
         runBlocking {
             repeat(1000) {
@@ -68,4 +66,74 @@ class CoroutineBasics : AppCompatActivity() {
         }
         println("Done......")
     }
+
+    // Unconfied dispatchers example
+    fun demoFour() {
+        runBlocking {
+            println("Run blocking  ${Thread.currentThread()}")
+            launch(Dispatchers.Unconfined) {
+                println("Before suspension ... ${Thread.currentThread()}")
+                delay(5000L)
+                println("After suspension ${Thread.currentThread()}")
+
+            }
+        }
+    }
+
+    //Print coroutine job from context element
+    fun demoFive() {
+        runBlocking {
+            launch {
+                delay(1000L)
+                println("This coroutine is running on job  ${coroutineContext.job}")
+            }
+        }
+    }
+
+
+    //Children of a coroutine
+
+    fun demoSix() {
+        runBlocking {
+
+            val request = launch {
+                launch {
+                    delay(100L)
+                    println("Child of REQUEST coroutine....")
+                    delay(1000L)
+                    println("Parent is cancelled so cancel this ....")
+                }
+                launch(Job()) {
+                    delay(100L)
+                    println("Independent coroutine...")
+                    delay(1000L)
+                    println("Still running after cancellation of  REQUEST coroutine ")
+                }
+            }
+
+            delay(500L)
+            request.cancel()
+            println("Cancelled the coroutine....")
+            delay(1000L)
+        }
+    }
+
+    //Parent coroutine waits for child coroutine to complete...
+    fun demoSeven() {
+        runBlocking {
+            val parent = launch {
+                repeat(5) { i ->
+                    launch {
+                        delay((i + 1) * 200L)
+                        println(i)
+                    }
+                }
+                println("Parent coroutine nothing to execute, just waiting for child coroutine to complete..")
+            }
+
+            parent.join()
+            println("Completed all the child coroutine....")
+        }
+    }
+
 }
