2700e077b3d720ebcb3d70249bdab12879052372
MPB <mp4learning@gmail.com>
2023-11-01 15:57:50 +0530

    Channel demo

--- app/src/main/java/com/example/kotlinapp/ChannelDemo.kt
+++ app/src/main/java/com/example/kotlinapp/ChannelDemo.kt
@@ -1,13 +1,10 @@
 package com.example.kotlinapp
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class ChannelDemo {
 
@@ -19,12 +16,46 @@ class ChannelDemo {
 
         val scope = CoroutineScope(Job())
 
+        //channel with buffer capacity 2 (i.e Buffer channel)
+        scope.launch {
+            receiveChannel = produce(capacity = 2) {
+                send(Language.python)
+                send(Language.kotlin)
+                send(Language.javascript)
+                send(Language.java)
+            }
+        }
+
+        //Conflated channel, capacity limit is 1 and override the previous element.
+        scope.launch {
+            receiveChannel = produce(capacity = Channel.CONFLATED) {
+                send(Language.python)
+                send(Language.kotlin)
+                send(Language.javascript)
+                send(Language.java)
+            }
+        }
+
+
+        //UNLIMITED channel,buffer capacity is unlimited
+        scope.launch {
+            receiveChannel = produce(capacity = Channel.UNLIMITED) {
+                send(Language.python)
+                send(Language.kotlin)
+                send(Language.javascript)
+                send(Language.java)
+            }
+        }
+
+        //Capacity zero , rendezvous
         scope.launch {
             receiveChannel = produce {
                 send(Language.python)
                 send(Language.javascript)
+                send(Language.kotlin)
             }
         }
+
 
         scope.launch {
             channel.send(Language.java)
@@ -44,6 +75,25 @@ class ChannelDemo {
                 println(it)
             }
         }
+
+        //receiver with channel capacity producer
+        scope.launch {
+            println("${receiveChannel.receive()}")
+            delay(3000)
+            println("${receiveChannel.receive()}")
+            delay(3000)
+            println("${receiveChannel.receive()}")
+            delay(3000)
+            println("${receiveChannel.receive()}")
+        }
+
+        //Receiver for Conflated channel
+        scope.launch {
+            receiveChannel.consumeEach {
+                println("${receiveChannel.receive()}") // this will receive only the last value from channel producer
+            }
+        }
+
     }
 
     enum class Language {
