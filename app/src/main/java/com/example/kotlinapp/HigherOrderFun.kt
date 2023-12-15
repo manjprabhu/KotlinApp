022502736532a686e31c3933054a7382bd46a2f9
MPB <mp4learning@gmail.com>
2023-12-15 14:02:52 +0530

    HigherOrder function Kotlin

--- app/src/main/java/com/example/kotlinapp/HigherOrderFun.kt
+++ app/src/main/java/com/example/kotlinapp/HigherOrderFun.kt
@@ -8,7 +8,12 @@ class HigherOrderFun : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        higherOrder(10, 20, ::add)
+        higherOrder(10, 20, ::addOne)
+
+        //passing lamda as param
+        higherOrder(10, 4) { a, b ->
+            a + b
+        }
 
         generateRandom(1..6, 4) { result ->
             println("Result : $result")
@@ -19,9 +24,11 @@ class HigherOrderFun : AppCompatActivity() {
         }
 
         println(res) // will print "Number generating"
+
+        calculatorDemo()
     }
 
-    private fun add(a: Int, b: Int): Int {
+    private fun addOne(a: Int, b: Int): Int {
         return a + b
     }
 
@@ -45,4 +52,41 @@ class HigherOrderFun : AppCompatActivity() {
         }
         return "Number generating"
     }
+
+    private fun calculatorDemo() {
+        val sum = calculate(10, 20, ::add)
+        println("==>> Sum is : $sum")
+
+        val diff = calculate(20, 10, ::subtract)
+        println("==>> Difference is : $diff")
+
+        val multi = calculate(20, 10, ::multiply)
+        println("==>> Multiplication is : $multi")
+
+        val result = calculate(20, 10, ::division)
+        println("==>> Division is : $result")
+    }
+
+    //One more example of HigherOrder function, calculator example
+
+    private fun calculate(paramOne: Int, paramTwo: Int, operation: (Int, Int) -> Int): Int {
+        return (operation(paramOne, paramTwo))
+    }
+
+    private fun add(one: Int, two: Int): Int {
+        return one + two
+    }
+
+    private fun subtract(one: Int, two: Int): Int {
+        return one - two
+    }
+
+    private fun multiply(one: Int, two: Int): Int {
+        return one * two
+    }
+
+    private fun division(one: Int, two: Int): Int {
+        return one / two
+    }
+
 }
