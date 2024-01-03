732dfbc819ef2862831122b2ecf8aaf7826f8e03
MPB <mp4learning@gmail.com>
2024-01-03 16:17:54 +0530

    Inline function

---
+++ app/src/main/java/com/example/kotlinapp/InlineFunctionActivity.kt
@@ -0,0 +1,55 @@
+package com.example.kotlinapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InlineFunctionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        calculatorDemo()
+    }
+
+    private fun testInline() {
+        println("==>> Test Inline ")
+
+        helloMethod()
+
+        helloMethod()
+
+        helloMethod()
+    }
+
+    private inline fun helloMethod() {
+        println("==>> Hello method")
+    }
+
+
+    //Usage in higerorder function
+    //Each higherorder function we create leads to function object creation and memory allocation, this introduces runtime overhead
+    //because of inline keyword , compiler copies the content of the function to call site, rather than creating new function object
+
+    // To decrease the memory allocation caused by lamda expression/ higerorder function use inline
+    //make sure inline is applied to small function that lamda as param
+
+    private fun calculatorDemo() {
+        val sum = calculate(10, 20, ::add)
+        println("==>> Sum is : $sum")
+
+        val diff = calculate(20, 10, ::subtract)
+        println("==>> Difference is : $diff")
+    }
+
+    private inline fun calculate(paramOne: Int, paramTwo: Int, operation: (Int, Int) -> Int): Int {
+        return (operation(paramOne, paramTwo))
+    }
+
+    private fun add(one: Int, two: Int): Int {
+        return one + two
+    }
+
+    private fun subtract(one: Int, two: Int): Int {
+        return one - two
+    }
+
+}
